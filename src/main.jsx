3fed078b6fc4f3b9763d73ad970e3f704ceb5af6
janhavi2912 <janhaavidhumal18@gmail.com>
2025-07-23 15:21:14 +0530

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App.jsx';
+
+// Replace 'Plant_Disease_Checker' with your actual repository name
+const basename = '/Plant_Disease_Checker';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter basename={basename}>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+);
--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),
-  tailwindcss(),]
+  tailwindcss(),],
+  base: "Plant_Disease_Checker" ,
+
 
 })
